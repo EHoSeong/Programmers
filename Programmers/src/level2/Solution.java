30646d5a6daa80a5b97c5eea142df5f02c949d15
Jjangguisdryingclothes <lhs06185@nate.com>
2024-11-20 20:59:32 +0900

    solution

---
+++ Programmers/src/level2/Solution.java
@@ -0,0 +1,21 @@
+package level2;
+
+import java.util.Arrays;
+
+class Solution {
+	public int solution(int[][] targets) {
+		Arrays.sort(targets, (o1, o2) -> o1[1] - o2[1]);
+
+		int answer = 0;
+		int shoot = 0;
+		for (int i = 0; i < targets.length; i++) {
+			if (shoot > targets[i][0]) {
+				continue;
+			} else {
+				answer++;
+				shoot = targets[i][1]; 
+			}
+		}
+		return answer;
+	}
+}
